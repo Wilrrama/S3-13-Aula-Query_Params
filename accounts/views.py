d93ab5700bc68a8d854b4e91c5f665c49b594778
wilrrama <wilrrama@hotmail.com>
2023-10-15 21:28:14 -0300

    aula finalizada

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 # accounts/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from .serializers import AccountSerializer
 
 from .models import Account
@@ -8,4 +9,13 @@ from .models import Account
 
 class AccountFindView(APIView):
     def get(self, request):
-        ...
+        account_balance = request.query_params.get("account_balance_gte", None)
+        gender = request.query_params.get("gender", None)
+
+        accounts = Account.objects.filter(
+            account_balance__gte=account_balance, gender=gender
+        )
+
+        serializer = AccountSerializer(accounts, many=True)
+
+        return Response(serializer.data)
